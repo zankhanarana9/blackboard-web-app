c8b696e6747c6fd57956ecd43182db0521f100f3
zankhana rana <zankhanarana9@gmail.com>
2021-01-06 00:09:35 -0600

    Updated courses.json and added Module and Topics Service

--- src/components/course-editor/Widget.js
+++ src/components/course-editor/Widget.js
@@ -5,20 +5,6 @@ const Widget = function() {
         <div className="widget border border-dark mt-3 mb-3">
               
         <div className="row mt-2 p-2">
-<<<<<<< HEAD
-          <div className="col-md-6 col-12">
-            <h3>Heading widget</h3>
-          </div>
-          <div className="col-md-2 col-12">
-            <div className="row">
-                <div className="col">
-                    <button className="btn btn-warning mr-2">
-                        <i className="fa fa-arrow-up"></i>
-                    </button>
-                </div>
-                <div className="col">
-                    <button className="btn btn-warning ml-2">
-=======
           <div className="col-md-5 text-left">
             <h3>Heading widget</h3>
           </div>
@@ -31,17 +17,12 @@ const Widget = function() {
                 </div>
                 <div className="col-1">
                     <button className="btn btn-warning">
->>>>>>> react
                         <i className="fa fa-arrow-down"></i>
                     </button>
                 </div>
             </div>
           </div>
-<<<<<<< HEAD
-          <div className="col-md-3 col-12">
-=======
           <div className="col-md-3">
->>>>>>> react
             <select className="form-control" id="widgetType">
               <option> Heading</option>
               <option >Paragraph</option>
@@ -50,11 +31,7 @@ const Widget = function() {
               <option>Link</option>
             </select>
           </div>
-<<<<<<< HEAD
-          <div className="col-md-1 col-12">
-=======
           <div className="col-md-2 text-right">
->>>>>>> react
             <button className="btn btn-danger">X</button>
           </div>
         </div>
